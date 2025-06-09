0c24e4f06c010d3df083fbfb9fc79d1604c5a2a0
Matias Chomicki <matyax@gmail.com>
2025-06-09 18:59:50 +0200

    LogListSearch: implement optional results filtering

--- public/app/features/logs/components/panel/LogList.tsx
+++ public/app/features/logs/components/panel/LogList.tsx
@@ -29,7 +29,7 @@ import { GetRowContextQueryFn, LogLineMenuCustomItem } from './LogLineMenu';
 import { LogListContextProvider, LogListState, useLogListContext } from './LogListContext';
 import { LogListControls } from './LogListControls';
 import { LogListSearch } from './LogListSearch';
-import { LogListSearchContextProvider } from './LogListSearchContext';
+import { LogListSearchContextProvider, useLogListSearchContext } from './LogListSearchContext';
 import { preProcessLogs, LogListModel } from './processing';
 import { useKeyBindings } from './useKeyBindings';
 import {
@@ -251,6 +251,7 @@ const LogListComponent = ({
   const styles = getStyles(dimensions, { showTime }, theme);
   const widthContainer = wrapperRef.current ?? containerElement;
   useKeyBindings();
+  const { filterLogs, matchingUids } = useLogListSearchContext();
 
   const debouncedResetAfterIndex = useMemo(() => {
     return debounce((index: number) => {
@@ -354,11 +355,11 @@ const LogListComponent = ({
     debouncedResetAfterIndex(0);
   }, [debouncedResetAfterIndex]);
 
-  const filteredLogs = useMemo(
-    () =>
-      filterLevels.length === 0 ? processedLogs : processedLogs.filter((log) => filterLevels.includes(log.logLevel)),
-    [filterLevels, processedLogs]
-  );
+  const filteredLogs = useMemo(() => {
+    const filteredLogs =
+      filterLevels.length === 0 ? processedLogs : processedLogs.filter((log) => filterLevels.includes(log.logLevel));
+    return matchingUids && filterLogs ? filteredLogs.filter((log) => matchingUids.includes(log.uid)) : filteredLogs;
+  }, [filterLevels, filterLogs, matchingUids, processedLogs]);
 
   return (
     <div className={styles.logListContainer}>
